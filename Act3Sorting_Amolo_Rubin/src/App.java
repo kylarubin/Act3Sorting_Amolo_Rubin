71e5ef546cb2a8b16ee74f2dfdfe398d4e4fe3f5
kylarubin <kyla_abigail.rubin@bisu.edu.ph>
2025-04-07 12:26:04 +0800

    Add files via upload

---
+++ Act3Sorting_Amolo_Rubin/src/App.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+class Student {
+    String name;
+    double grade;
+
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String toString() {
+        return name + " - " + grade;
+    }
+}
+
+public class App {
+
+    public static void bubbleSort(Student[] arr, boolean ascending) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (ascending) {
+                    if (arr[j].grade > arr[j + 1].grade) {
+                        Student temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                } else {
+                    if (arr[j].grade < arr[j + 1].grade) {
+                        Student temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many students? ");
+        int numStudents = scanner.nextInt();
+        scanner.nextLine(); 
+
+        Student[] students = new Student[numStudents];
+        for (int i = 0; i < numStudents; i++) {
+            System.out.println("Enter Details for Student #" + (i + 1));
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Grade: ");
+            double grade = scanner.nextDouble();
+            scanner.nextLine(); 
+            students[i] = new Student(name, grade);
+        }
+
+        bubbleSort(students, true); 
+
+        System.out.println("\nSorted Students by Grade (Ascending):");
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        scanner.close();
+    }
+}
